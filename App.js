64a32cecc5a1361aaa9d671ba55d5fcc191e249e
Joshua Akangah <47978604+kangah-codes@users.noreply.github.com>
2021-10-14 07:43:07 +0000

    added app.js

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Constants from "expo-constants";
+import { useFonts } from "expo-font";
+
+import Login from "./screens/Login";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	let [fontsLoaded] = useFonts({
+		"Cereal-Regular": require("./assets/fonts/AirbnbCerealMedium.ttf"),
+	});
+
+	if (!fontsLoaded) {
+		return null;
+	} else {
+		return (
+			<View style={styles.container}>
+				<Login />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		marginTop: Constants.statusBarHeight,
+		fontFamily: "Cereal-Regular",
+	},
 });
